349d92d848df7a0edf322e9e34388321a3528feb
ndorrh <ndorrh@gmail.com>
2023-05-03 13:59:33 +0100

    Set up authentication with google using next-auth

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
+        <p> Signed in as {session.user.email}</p>
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center justify-center">
-     <div className="flex gap-2">
-        <button className="bg-white p-4 rounded-lg">Login with Google</button>
-        <button className="bg-white p-4 rounded-lg">Login with Facebook</button>
-     </div>
+    <div className="bg-blue-900 w-screen h-screen flex items-center justify-center flex-col">
+      <p>  Not signed in</p>
+      <div className="flex gap-2">
+        <button className="bg-white p-4 rounded-lg" onClick={() => signIn('google')}>Login with Google</button>
+      </div>
     </div>
   )
 }
