23318a58d466b7ffafc4e9cef48007005aa5ec9c
ndorrh <ndorrh@gmail.com>
2023-05-12 17:12:00 +0100

    Add order component

---
+++ pages/orders.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+import HalfRedCircle from "@/components/halfRedCircle";
+
+export default function Orders() {
+  return (
+    <Layout>
+      fine me in orders
+<HalfRedCircle />
+    </Layout>
+  )
+}
